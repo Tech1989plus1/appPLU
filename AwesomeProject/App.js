e88b9082315e1ed3531f970ce5a492b071134cba
Derrick Marshall <marshallderrickdnm@gmail.com>
2019-05-16 21:08:52 -0700

    index

---
+++ AwesomeProject/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import {styles} from './style/appStyles'
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text style= {styles.welcome}>PLU'S</Text>
+        <Text style = {styles.instructions}>by Derrick Marshall</Text>
+      </View>
+    );
+  }
+}
+
